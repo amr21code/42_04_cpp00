b228c38ed67c6dd94866abfa3946303f24050ee2
amr21code <andi@noshut.de>
2022-07-10 16:33:54 +0200

    setup

---
+++ ex01/PhoneBook.class.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.class.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anruland <anruland@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/10 16:31:00 by anruland          #+#    #+#             */
+/*   Updated: 2022/07/10 16:31:41 by anruland         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_CLASS_HPP
+# define PHONEBOOK_CLASS_HPP
+
+class PhoneBook {
+
+};
+
+#endif
